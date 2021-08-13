3e977aeff88ef4d23ee1c8a008d808d19e98755f
Jonathan Ganem <jonathan.ganem@eficia.com>
2021-08-13 16:47:36 +0200

    Maj username not showing if user don't have a nickname

--- src/Actions/Recruitment.php
+++ src/Actions/Recruitment.php
@@ -14,14 +14,17 @@ class Recruitment
     public static function actions($discord, $message)
     {
         $member = $message->author;
-        $nickname = strtolower(str_replace(' ', '-', $member->nick));
+        $name = ($member->nick == "") ? $member->username : $member->nick;
+
+        $name = strtolower(str_replace(' ', '-', $name));
+
         $guildId = $message->channel->guild_id;
         $guild = $discord->guilds->get('id', $guildId);
         // $guild->createRole([
         //     'name' => "recrutement-{$nickname}"
         // ]);
         $channel = $guild->channels->create([
-            'name' => "recrutement-{$nickname}",
+            'name' => "recrutement-{$name}",
             'type' => 0,
             'parent_id' => $_ENV['RECRUITMENT_CHANNEL_ID'],
             'is_private' => true
@@ -32,13 +35,13 @@ class Recruitment
                 'view_channel',
                 'read_message_history'
             ]);
-            $channel->sendMessage("Hello {$member}\n".
-            "Peux-tu te présenter en quelques points :\n".
-            "1) Comment as-tu connu la corporation et pourquoi souhaites-tu la rejoindre ?\n".
-            "2) Comment a été ton expérience de Eve jusqu'à maintenant ? Quel est ton objectif en jeu actuellement ? As-tu déjà rejoint d'autres corporations ? Pourquoi les as-tu rejoint et les as-tu quitté ? Joues-tu à d'autres jeux à part Eve ?\n".
-            "3) As-tu déjà PvP auparavant ? Quels sont les ships que tu aimes piloter ?\n".
-            "4) Quels sont tes horaires de jeu ? A quelles heures es-tu le plus souvent connecté ?\n".
-            "5) Comment te décris-tu ? Quel est ton caractère ?\n");
+            $channel->sendMessage("Hello {$member}\n" .
+                "Peux-tu te présenter en quelques points :\n" .
+                "1) Comment as-tu connu la corporation et pourquoi souhaites-tu la rejoindre ?\n" .
+                "2) Comment a été ton expérience de Eve jusqu'à maintenant ? Quel est ton objectif en jeu actuellement ? As-tu déjà rejoint d'autres corporations ? Pourquoi les as-tu rejoint et les as-tu quitté ? Joues-tu à d'autres jeux à part Eve ?\n" .
+                "3) As-tu déjà PvP auparavant ? Quels sont les ships que tu aimes piloter ?\n" .
+                "4) Quels sont tes horaires de jeu ? A quelles heures es-tu le plus souvent connecté ?\n" .
+                "5) Comment te décris-tu ? Quel est ton caractère ?\n");
         });
     }
 }
