2cb03ce6b9e5cd3e38967b76a1cec2b625eff8b2
Jonathan Ganem <11046515+JohnGanem@users.noreply.github.com>
2026-02-09 00:31:14 +0100

    - Better README - Added possibility to transfer message to a webhook - Reworked Initialisation.php and .env to let the user choose actions needed

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ ZigZag est un bot discord qui a été utilisé dans le cadre de la corporation L
 - Créer un salon privé pour la recrue afin d'avoir un espace pour échanger avec elle
 - Notifier les membres du discord de l'arrivée de cette recrue
 
+Le bot a été étendu pour ajouter la possibilité de réaliser un trigger n8n sur la réception d'un message Discord.
+
 L'utilisation du bot est à la demande et à la validation du créateur uniquement.
+
+Pour lancer le bot, lancer main.php. Peut se faire dans un cron avec @reboot ex:
+```@reboot php /home/<user>/ZigZag/main.php```
---
+++ src/Actions/TransferMessages.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZigZag\Actions;
+
+class TransferMessages
+{
+    public static function actions($message)
+    {
+        if($_ENV['NEED_AUTHOR_ID'] && $_ENV['NEED_AUTHOR_ID'] != $message->author->id) {
+            return;
+        }
+        // stuff it into a query
+        $query = http_build_query($message);
+        // curl my data into the zap
+        $ch = curl_init($_ENV['WEBHOOK_URL']);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+    }
+}
